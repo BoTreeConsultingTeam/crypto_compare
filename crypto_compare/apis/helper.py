f36f1f50144e3e1689f37093557691e8a16b3321
miscreanity <miscreanity@gmail.com>
2019-07-18 10:33:23 -0400

    Update for Python3 & include CC API key option

--- crypto_compare/apis/helper.py
+++ crypto_compare/apis/helper.py
@@ -1,5 +1,5 @@
 import json
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 
 ALLOWED_KEYS = [
     'fsym',
@@ -8,16 +8,19 @@ ALLOWED_KEYS = [
     'tsyms',
     'ts',
     'e',
+    'limit',
     'markets',
     'extraParams',
     'sign',
+    'tots',
+    'toTs',
     'tryConversion',
     'calculationType'
 ]
 
 def _is_params_valid(self, **kwargs):
 
-    for key,value in kwargs.items():
+    for key,value in list(kwargs.items()):
         if(value == None or value == ''):
             raise ValueError(key+' cannot be empty!')
 
@@ -28,7 +31,7 @@ def _fetch_data(self, url):
 
     print('Calling URL - ' + url)
     
-    json_response = json.load(urllib2.urlopen(url))
+    json_response = json.load(urllib.request.urlopen(url))
     
     if 'Response' in json_response and json_response["Response"] == 'Error':
         raise ValueError("API Error - {} !".format(json_response['Message']))
@@ -43,7 +46,7 @@ def _get_querystring(self, kwargs):
 
     if kwargs is not None:
         
-        for key,value in kwargs.items():
+        for key,value in list(kwargs.items()):
             
             if key == "fsym" or key == "fsyms":
                 fsym = value
@@ -53,4 +56,4 @@ def _get_querystring(self, kwargs):
             if key in ALLOWED_KEYS:
                 querystring = "{}&{}".format(querystring, "{}={}".format(key, value))
             
-    return fsym, tsym, querystring
+    return fsym, tsym, querystring + '&apikey=' + self.apikey
